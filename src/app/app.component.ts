e67d5bb50da263056a0e3408f69668c6829eeaa7
Jacobus Harding <jacobus.harding@metafuse.com>
2017-02-16 13:45:54 -0800

    second

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,7 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
+import {AngularFire, FirebaseListObservable, AuthProviders, AuthMethods} from 'angularfire2';
+import {Observable} from 'rxjs/Observable';
+import 'rxjs/add/operator/map'; 
 
 @Component({
   selector: 'app-root',
@@ -7,4 +10,102 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'app works!';
+  cuisines: FirebaseListObservable<any[]>;
+  restaurants: Observable<any[]>;
+
+  constructor(private af: AngularFire)
+  {
+  
+  }
+
+  ngOnInit() {
+    this.cuisines = this.af.database.list('/cuisines');
+    
+    //simple join
+    this.restaurants = this.af.database.list('/restaurants')
+      .map(restaurants => {
+        restaurants.map(restaurant => {
+          restaurant.cuisineType = this.af.database.object('/cuisines/' + restaurant.cuisine)
+        })
+        return restaurants;
+      })
+
+    //complex join
+    // this.restaurants = this.af.database.list('/restaurants')
+    //   .map(restaurants => {
+    //     restaurants.map(restaurant => {
+    //       restaurant.featureTypes = [];
+    //       for(var f in restaurant.features)
+    //       {
+    //         restaurant.featureTypes.push(this.af.database.object('/features/' + f));
+    //       }
+    //     })
+
+    //     return restaurants;
+    //   })
+
+      ////query objects
+      // this.restaurants = this.af.database.list('/restaurants', {
+      //   query: {
+      //     orderByChild: 'rating',
+      //     startAt: 3,
+      //     limitToFirst: 50
+      //   }
+      // })
+
+      //updating multiple
+      // this.af.database.list('/restaurants').push({name: ''})
+      //   .then(x => {
+      //     let restaurant = {name: 'My New restaurant'};
+      //     let update = {};
+
+      //     update['restaurants/' + x.key] = restaurant;
+      //     update['restaurants-by-city/camberwell/' + x.key] = restaurant;
+      //   })
+
+      this.af.auth.subscribe(authState => {
+        if(!authState)
+        {
+          console.log("not logged in");
+        }
+        else
+        {
+          console.log("logged in", authState);
+        }
+      })
+  }
+
+  add()
+  {
+    this.cuisines.push({
+      name: 'Asian',
+      details: {
+        description: '...'
+      }
+    })
+  }
+
+  update()
+  {
+    this.af.database.object('/restaurant').update({
+      name: 'New name',
+      rating: 5
+    })
+  }
+
+  login()
+  {
+    this.af.auth.login({
+      provider: AuthProviders.Facebook,
+      method: AuthMethods.Popup
+    }).then(authState => {
+      console.log("After Login", authState);
+    })
+  }
+
+  logout()
+  {
+    this.af.auth.logout();
+  }
+
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,8 +2,17 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
+import {AngularFireModule} from 'angularfire2';
 
 import { AppComponent } from './app.component';
+
+export const firebaseConfig = {
+    apiKey: "AIzaSyASmkJs4phvlGjH4ySgrqD7FxRlZWrtw2Y",
+    authDomain: "epper-4d1a7.firebaseapp.com",
+    databaseURL: "https://epper-4d1a7.firebaseio.com",
+    storageBucket: "epper-4d1a7.appspot.com",
+    messagingSenderId: "262321847467"
+}
 
 @NgModule({
   declarations: [
@@ -12,7 +21,8 @@ import { AppComponent } from './app.component';
   imports: [
     BrowserModule,
     FormsModule,
-    HttpModule
+    HttpModule,
+    AngularFireModule.initializeApp(firebaseConfig)
   ],
   providers: [],
   bootstrap: [AppComponent]
