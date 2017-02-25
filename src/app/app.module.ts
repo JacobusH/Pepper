cf52f9bad199636725af7fac9d52d74886c8e915
Jacobus Harding <jacobus.harding@metafuse.com>
2017-02-24 16:34:44 -0800

    basic routing

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,7 @@ import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 import {AngularFireModule} from 'angularfire2';
+import {RouterModule, Routes} from '@angular/router';
 
 import { AppComponent } from './app.component';
 
